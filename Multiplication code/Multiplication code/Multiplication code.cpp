54f76a201afa0026f01b1d77c20960dfe250fe8c
Vansh-jadeja <73414858+Vansh-jadeja@users.noreply.github.com>
2021-04-17 20:47:59 -0700

    improve input output messages

--- Multiplication code/Multiplication code/Multiplication code.cpp
+++ Multiplication code/Multiplication code/Multiplication code.cpp
@@ -3,16 +3,19 @@ using namespace std;
 
 int main()
 {
+    
+        cout << "This program will multiply 2 numbers then print out the answer. The first step is to type in two diffrent numbers(make  sure to have space between the two numbers), then press enter." << endl;
+    
     double firstNumber, secondNumber, productOfTwoNumbers;
-    cout << "";
-
+    
     // Stores two floating point numbers in variable firstNumber and secondNumber respectively
     cin >> firstNumber >> secondNumber;
-
+    cout << "You have entered: " << firstNumber;
+    cout << "x" << secondNumber << endl;
     // Performs multiplication and stores the result in variable productOfTwoNumbers
     productOfTwoNumbers = firstNumber * secondNumber;
 
-    cout << "The answer is:" << productOfTwoNumbers;
+    cout << "The answer is: " << productOfTwoNumbers << endl;
 
     return 0;
 }
