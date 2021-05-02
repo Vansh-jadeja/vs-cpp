cbef5a27c2fb183302f30fd34f7e583f29103752
Vansh-jadeja <73414858+Vansh-jadeja@users.noreply.github.com>
2021-05-01 18:54:09 -0700

    Zombie Apocalypse - Text Game

---
+++ Z-Apocalypse/Z-Apocalypse/Z-Apocalypse.cpp
@@ -0,0 +1,19 @@
+// Z-Apocalypse.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream> 
+using namespace std;
+
+void intro()
+{
+    cout << "Intro- You are a scientist survivor trapped in a Zombie apocalypes. Everyone you knew is dead or turned into zombies.   The only way to save the world is to go to the science station no.3, the only problem, its's located on the ocean, near the coast of new york." 
+<< endl;
+}
+
+int main()
+{
+    intro () ;
+    char k; 
+    cin >> k;
+}
+
