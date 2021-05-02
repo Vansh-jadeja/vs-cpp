53468087e59b09d5ebf4655dabff98bc3c0fe643
Vansh-jadeja <73414858+Vansh-jadeja@users.noreply.github.com>
2021-05-01 19:12:48 -0700

    First course of action

--- Z-Apocalypse/Z-Apocalypse/Z-Apocalypse.cpp
+++ Z-Apocalypse/Z-Apocalypse/Z-Apocalypse.cpp
@@ -9,11 +9,26 @@ void intro()
     cout << "Intro- You are a scientist survivor trapped in a Zombie apocalypes. Everyone you knew is dead or turned into zombies.   The only way to save the world is to go to the science station no.3, the only problem, its's located on the ocean, near the coast of new york." 
 << endl;
 }
+int firstAction()
+{
+    cout << "What is Your first course of action?" << endl;
+    cout << "Opt.1 charge the batteries and fill your bottles up with water" << endl;
+    cout << "Opt.2 Make weapons" << endl;
 
+    cout << "ENter your choice: ";
+    int choice;
+    cin >> choice;
+
+    cout << "You have chosen: " << choice << endl;
+    return choice;
+}
 int main()
 {
-    intro () ;
-    char k; 
+    intro();
+
+    firstAction();
+
+    //wait for user to press a key
+    char k;
     cin >> k;
 }
-
